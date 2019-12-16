7eefd6388a409bd3e6060fb9a1ffd25171fd6a14
Grove Pyree <grayarea@protonmail.ch>
2019-12-16 15:51:47 +0300

    Remove flags from dpl

--- macos-local-server/macos-local-server.dpl.sh
+++ macos-local-server/macos-local-server.dpl.sh
@@ -2,14 +2,14 @@
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
 #:revdate:      2019.12.16
-#:revremark:    Improve handling of certificate links
+#:revremark:    Remove flags from dpl
 #:created_at:   2019.06.30
 
 D_DPL_NAME='macos-local-server'
 D_DPL_DESC='macOS web-dev server (httpd, php, mariadb, dnsmasq)'
 D_DPL_PRIORITY=4096
-D_DPL_FLAGS=!ir
-D_DPL_WARNING='Make sure you know what you are doing'
+D_DPL_FLAGS=
+D_DPL_WARNING=
 D_DPL_OS=( macos )
 
 
